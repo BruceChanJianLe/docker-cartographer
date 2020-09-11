8386830105f4d37182723ff3c028ac53421711f6
brucechanjianle <jianle001@e.ntu.edu.sg>
2020-09-11 12:13:25 +0800

    add start container script

---
+++ start_docker_container.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+# This script start the docker container
+
+echo -e "Starting container image ubuntu18.04:cartographer"
+read -p "Container name: " CONTAINERNAME
+
+docker run -d -ti -v $(pwd)/..:/root/docker_mount --net=host --name $CONTAINERNAME --cap-add=SYS_PTRACE ubuntu18.04:cartographer
